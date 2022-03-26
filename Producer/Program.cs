a6ddf9174dbcd3ac570f546cd81873945a05e2ff
igor egorov <igor127@bk.ru>
2022-03-26 18:04:50 +0300

    Add project files.

---
+++ Producer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Producer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
